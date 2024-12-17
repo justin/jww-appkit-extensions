cce5e70a0c839cdd9f1400147ec1f99d2d833095
Justin Williams <justinw@me.com>
2024-12-17 06:03:05 -0700

    Add NSView autolayout extensions

---
+++ Sources/JWWAppKit/NSView+Extensions.swift
@@ -0,0 +1,12 @@
+import AppKit
+
+extension NSView {
+    /// Boolean that returns true if the view is using Auto Layout. False otherwise.
+    ///
+    /// This is merely a convenience wrapper around `translatesAutoresizingMaskIntoConstraints` that inverts the
+    /// logic.
+    public var usesAutoLayout: Bool {
+        get { return !translatesAutoresizingMaskIntoConstraints }
+        set { translatesAutoresizingMaskIntoConstraints = !newValue }
+    }
+}
--- Tests/JWWAppKitTests/JWWAppKitTests.swift
+++
@@ -1,6 +0,0 @@
-import Testing
-@testable import JWWAppKit
-
-@Test func example() async throws {
-    #expect(true)
-}
